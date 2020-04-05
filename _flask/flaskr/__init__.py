8b78b5190ebbf8c286f69bbe84fac9fb17885b58
oongie <35512857+Oongie@users.noreply.github.com>
2020-04-05 15:29:29 +1200

    Depreciate tutorial app

--- _flask/flaskr/__init__.py
+++ _flask/flaskr/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 
 def create_app(test_config=None):
-    "Create an configure the app"
+    "Create and configure the app"
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
